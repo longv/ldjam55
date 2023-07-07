fdd80bdecf2cfd4d6c98e00f366c252cfdcbfa40
BaoCaiH <caihongbao280996@gmail.com>
2023-07-07 13:46:02 +0300

    add background and tilemap

--- Players/Player.gd
+++ Players/Player.gd
@@ -6,8 +6,9 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var anim = get_node("AnimatedSprite2D")
 
+@onready var anim = get_node("AnimationPlayer")
+@onready var sprite = get_node("AnimatedSprite2D")
 
 func _ready():
 	anim.play("Idle")
@@ -25,9 +26,18 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	sprite.flip_h = true if direction == -1 else false if direction == 1 else sprite.flip_h 
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			anim.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			anim.play("Idle")
+	if velocity.y < 0:
+		anim.play("Jump")
+	elif velocity.y > 0:
+		anim.play("Fall")
 
 	move_and_slide()
