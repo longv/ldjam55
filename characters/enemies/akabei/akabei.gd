29bfa21fee96619a00e0ca929b7e508d28e9126b
Long Vu <longv@longvs-mbp.lan>
2024-04-13 20:50:41 +0300

    Implement moving transition

--- characters/enemies/akabei/akabei.gd
+++ characters/enemies/akabei/akabei.gd
@@ -10,25 +10,27 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _process(delta):
-	_update_anim(velocity.normalized())
+	_update_anim()
 
 func _physics_process(delta):
-	if velocity.y == 0:
-		var h_direction = Input.get_axis("ui_left", "ui_right")
-		if h_direction:
-			velocity.x = h_direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+	var h_direction = Input.get_axis("ui_left", "ui_right")
+	if h_direction && velocity.y == 0:
+		velocity.x = h_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if velocity.x == 0:
-		var v_direction = Input.get_axis("ui_up", "ui_down")
-		if v_direction:
-			velocity.y = v_direction * SPEED
-		else:
-			velocity.y = move_toward(velocity.y, 0, SPEED)
+	var v_direction = Input.get_axis("ui_up", "ui_down")
+	if v_direction && velocity.x == 0:
+		velocity.y = v_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
 
-func _update_anim(direction: Vector2):
-	if direction != Vector2.ZERO:
-		anim_tree.set("parameters/Idle/blend_position", direction)
+func _update_anim():
+	if velocity != Vector2.ZERO:
+		anim_tree["parameters/Idle/blend_position"] = velocity.normalized()
+		anim_tree["parameters/Run/blend_position"] = velocity.normalized()
+
+	anim_tree["parameters/conditions/idling"] = velocity == Vector2.ZERO
+	anim_tree["parameters/conditions/moving"] = velocity != Vector2.ZERO
