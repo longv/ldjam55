e15a21b39237dc07fd7944cee21ec4bdfc58c363
BaoCaiH <caihongbao280996@gmail.com>
2024-04-13 23:39:46 +0300

    Add scene and enemy switcher

--- characters/enemies/annoying_colleagues/colleague.gd
+++ characters/enemies/annoying_colleagues/colleague.gd
@@ -7,22 +7,34 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = get_node("AnimationPlayer")
+@onready var task_scene = load("res://objects/task.tscn")
+@onready var task_appear: Node2D = get_node("TaskAppear")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# # Handle jump.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# 	velocity.y = JUMP_VELOCITY
+	#
+	# # Get the input direction and handle the movement/deceleration.
+	# # As good practice, you should replace UI actions with custom gameplay actions.
+	# var direction = Input.get_axis("ui_left", "ui_right")
+	# if direction:
+	# 	velocity.x = direction * SPEED
+	# else:
+	# 	velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if position.x >= 877:
+		velocity.x = -SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.x > 0:
+		anim.play("run")
+	else:
+		anim.play("idle")
 
 	move_and_slide()
--- levels/main.gd
+++ levels/main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var counter: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +9,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	counter += 1
+	if int(counter) % 100 == 0:
+		print(counter)
 
 
 func _on_quit_pressed():
@@ -16,9 +19,16 @@ func _on_quit_pressed():
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://levels/world.tscn")
+	# get_tree().change_scene_to_file("res://levels/meeting.tscn")
+	# var s = ResourceLoader.load("res://levels/meeting.tscn")
+	if SceneSwitcher.annoyance == "colleague":
+		SceneSwitcher.annoyance = "akabei"
+	elif SceneSwitcher.annoyance == "akabei":
+		SceneSwitcher.annoyance = "aosuke"
+	else:
+		SceneSwitcher.annoyance = "colleague"
+	SceneSwitcher.goto_scene("res://levels/meeting.tscn")	
 
 
 func _on_play_mouse_entered():
-#	get_node("Play").(Vector2(4.1, 4.1))
 	pass
