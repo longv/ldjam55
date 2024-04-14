f6b1b6aa7422ff57f8f36970ff6b0088da57d0ed
BaoCaiH <caihongbao280996@gmail.com>
2024-04-14 17:04:54 +0300

    Add 4 enemy behaviour

---
+++ characters/enemies/annoying_colleagues/akabei.gd
@@ -0,0 +1,7 @@
+extends "res://characters/enemies/annoying_colleagues/colleague.gd"
+
+
+func _ready():
+	atk_speed = 650
+	tshirt_size = 6
+
--- characters/enemies/annoying_colleagues/colleague.gd
+++ characters/enemies/annoying_colleagues/colleague.gd
@@ -6,6 +6,10 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var counter:int = 0
+var atk_speed: int = 100
+var tshirt_size: int = 9
+var capacity: int = 10
 
 @onready var anim = get_node("AnimationPlayer")
 @onready var task_scene = load("res://objects/task.tscn")
@@ -15,18 +19,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# # Handle jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
-	#
-	# # Get the input direction and handle the movement/deceleration.
-	# # As good practice, you should replace UI actions with custom gameplay actions.
-	# var direction = Input.get_axis("ui_left", "ui_right")
-	# if direction:
-	# 	velocity.x = direction * SPEED
-	# else:
-	# 	velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	if position.x >= 877:
 		velocity.x = -SPEED
@@ -38,3 +30,19 @@ func _physics_process(delta):
 		anim.play("idle")
 
 	move_and_slide()
+
+func _process(_delta):
+	if capacity <= 0:
+		SceneSwitcher.goto_previous()
+	counter += 1
+	if int(counter) % atk_speed == 0:
+		counter = 0
+		send_task()
+
+func send_task():
+	var task = task_scene.instantiate()
+	task.position = task_appear.position
+	task.speed = -800.0
+	task.tshirt_size = tshirt_size
+	call_deferred("add_child", task)
+
