c76c20a7b396dafd4995e5c95f6c49a67081da9e
Long Vu <longv@longvs-mbp.lan>
2024-04-14 16:18:49 +0300

    Create base classes

---
+++ characters/enemies/enemy.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+class_name Enemy
+
+@export var speed = 300.0
+@export var acceleration = 7
+
+@onready var target = %KuchiNorm
+@onready var anim_tree = $AnimationTree
+@onready var nav_agent = $Navigation/NavigationAgent2D
+
+
+func _process(_delta):
+	_update_anim()
+
+func _physics_process(delta):
+	if nav_agent.is_target_reached():
+		velocity = velocity.move_toward(Vector2.ZERO, speed)
+	else:
+		var direction = nav_agent.get_next_path_position() - global_position
+		direction = direction.normalized()
+		velocity = velocity.lerp(direction * speed, acceleration * delta)
+
+	move_and_slide()
+
+func _update_anim():
+	if velocity != Vector2.ZERO:
+		anim_tree["parameters/Idle/blend_position"] = velocity.normalized()
+		anim_tree["parameters/Run/blend_position"] = velocity.normalized()
+
+	anim_tree["parameters/conditions/idling"] = velocity == Vector2.ZERO
+	anim_tree["parameters/conditions/moving"] = velocity != Vector2.ZERO
+
+
+func _on_timer_timeout():
+	if target:
+		nav_agent.target_position = target.global_position
