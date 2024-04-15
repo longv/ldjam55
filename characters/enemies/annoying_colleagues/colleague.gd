6222ed678fa3e7c1e77e961ce118a258a4dab0b6
Long Vu <15876242+longv@users.noreply.github.com>
2024-04-15 23:54:42 +0300

    Add aosuke and guzuta

--- characters/enemies/annoying_colleagues/colleague.gd
+++ characters/enemies/annoying_colleagues/colleague.gd
@@ -35,7 +35,7 @@ func _physics_process(delta):
 
 func _process(_delta):
 	if capacity <= 0:
-		SceneSwitcher.goto_previous(["Akabei", "Pinky"])
+		SceneSwitcher.goto_previous(["Akabei", "Pinky", "Aosuke", "Guzuta"])
 	counter += 1
 	if int(counter) % atk_speed == 0:
 		counter = 0
---
+++ characters/enemies/guzuta/guzuta.gd
@@ -0,0 +1,18 @@
+extends Enemy
+
+
+func _get_target_position():
+	match current_mode:
+		Mode.CHASE:
+			return target.global_position
+		Mode.SCATTER:
+			return Vector2(64, 64)
+		Mode.FRIGHTENED:
+			return null
+		_:
+			return null
+
+func _on_hitbox_body_entered(body: Node2D):
+	if body is KuchiNorm:
+		SceneSwitcher.annoyance = "guzuta"
+		SceneSwitcher.goto_scene("res://levels/meeting.tscn")	
